a9af275be3f738b4404dff02224599f063b9e25a
Josh Broomberg <joshbroomberg@gmail.com>
2021-08-10 18:08:04 -0500

    add imperative update commands to sagemaker target

--- regsync/regsync/deploy/__init__.py
+++ regsync/regsync/deploy/__init__.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Dict, Set
 from regsync.types import Model, ModelVersion
 
 
@@ -7,3 +7,15 @@ class DeployTarget(ABC):
     @abstractmethod
     def list_models(self) -> List[Model]:
         pass
+    
+    @abstractmethod
+    def create_versions(self, new_versions: List[ModelVersion]):
+      pass
+    
+    @abstractmethod
+    def update_version_stage(self, updated_versions: List[Dict[ModelVersion, Set(str)]]):
+      pass
+    
+    @abstractmethod
+    def delete_versions(self, deleted_versions: List[ModelVersion]):
+      pass
