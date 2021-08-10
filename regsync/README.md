65d49cebd022cae9d147b2f72bc42cd116eb54a5
Kevin Flansburg <kevin.flansburg@dominodatalab.com>
2021-08-10 10:29:43 -0500

    Basic Python skeleton

---
+++ regsync/README.md
@@ -0,0 +1,1 @@
+# Domino Registry Sync
---
+++ regsync/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="regsync",
+    version="0.1.0",
+    author="Kevin Flansburg, Josh Broomberg",
+    author_email="kevin.flansburg@dominodatalab.com,josh.broomberg@dominodatalab.com",
+    description="Automatically configure serving infrastructure based on model registry.",
+    url="https://github.com/dominodatalab/domino-research/regsync",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+)
