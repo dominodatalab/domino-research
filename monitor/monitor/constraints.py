13bc1a67cb6ebf40d0a4837901392f8646128f8f
Kevin Flansburg <kevin.flansburg@dominodatalab.com>
2021-08-23 12:21:48 -0500

    Dataclasses for constraints baseline

---
+++ monitor/monitor/constraints.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass
+class NumericalConstraints:
+    is_non_negative: bool
+
+
+@dataclass
+class StringConstraints:
+    domains: List[str]
+
+
+@dataclass
+class DistributionConstraints:
+    # 'Enabled' | 'Disabled'
+    perform_comparison: str = "Enabled"
+    comparison_threshold: float = 0.1
+    # 'Simple' | 'Robust'
+    comparison_method: str = "Simple"
+
+
+@dataclass
+class MonitoringConfig:
+    distribution_constraints: DistributionConstraints
+    # 'Enabled' | 'Disabled'
+    evaluate_constraints: str = "Enabled"
+    # 'Enabled' | 'Disabled'
+    emit_metrics: str = "Enabled"
+    datatype_check_threshold: float = 0.1
+    domain_content_threshold: float = 0.1
+
+
+@dataclass
+class Feature:
+    name: str
+    # 'Fractional' | 'Integral' | 'String' | 'Unknown'
+    inferred_type: str
+    # denotes observed non-null value percentage
+    completeness: float
+    num_constraints: NumericalConstraints
+    string_constraints: StringConstraints
+    monitoringConfigOverrides: Optional[MonitoringConfig]
+
+
+@dataclass
+class Constraints:
+    features: List[Feature]
+    monitoring_config: MonitoringConfig
+    version: int = 0
--- monitor/monitor/statistics.py
+++ monitor/monitor/statistics.py
@@ -3,24 +3,55 @@ from typing import List, Optional
 
 
 @dataclass
-class Statistics:
-    dataset: Dataset
-    version: int = 0
-    features: List[Feature] = field(default_factory=list)
+class KLLSketchParameters:
+    c: float
+    k: float
 
 
 @dataclass
-class Dataset:
-    item_count: int
+class KLLBucket:
+    lower_bound: float
+    upper_bound: float
+    count: int
 
 
 @dataclass
-class Feature:
-    name: str
-    # 'Fractional' | 'Integral' | 'String'
-    inferred_type: str
-    numerical_statistics: Optional[NumericalStatistics]
-    string_statistics: Optional[StringStatistics]
+class KLLSketch:
+    parameters: KLLSketchParameters
+    data: List[List[float]]
+
+
+@dataclass
+class CategoryBucket:
+    value: str
+    count: int
+
+
+@dataclass
+class CategoricalDistribution:
+    buckets: List[CategoryBucket]
+
+
+@dataclass
+class KLLDistribution:
+    buckets: List[KLLBucket]
+    sketch: KLLSketch
+
+
+@dataclass
+class StringDistribution:
+    categorical: CategoricalDistribution
+
+
+@dataclass
+class NumericalDistribution:
+    kll: KLLDistribution
+
+
+@dataclass
+class CommonStatistics:
+    num_present: int
+    num_missing: int
 
 
 @dataclass
@@ -42,52 +73,21 @@ class StringStatistics:
 
 
 @dataclass
-class CommonStatistics:
-    num_present: int
-    num_missing: int
+class Feature:
+    name: str
+    # 'Fractional' | 'Integral' | 'String' | 'Unknown'
+    inferred_type: str
+    numerical_statistics: Optional[NumericalStatistics]
+    string_statistics: Optional[StringStatistics]
 
 
 @dataclass
-class NumericalDistribution:
-    kll: KLLDistribution
+class Dataset:
+    item_count: int
 
 
 @dataclass
-class KLLDistribution:
-    buckets: List[KLLBucket]
-    sketch: KLLSketch
-
-
-@dataclass
-class KLLBucket:
-    lower_bound: float
-    upper_bound: float
-    count: int
-
-
-@dataclass
-class KLLSketch:
-    parameters: KLLSketchParameters
-    data: List[List[float]]
-
-
-@dataclass
-class KLLSketchParameters:
-    c: float
-    k: float
-
-
-@dataclass
-class StringDistribution:
-    categorical: CategoricalDistribution
-
-
-@dataclass
-class CategoricalDistribution:
-    buckets: List[CategoryBucket]
-
-
-@dataclass
-class CategoryBucket:
-    value: str
-    count: int
+class Statistics:
+    dataset: Dataset
+    version: int = 0
+    features: List[Feature] = field(default_factory=list)
