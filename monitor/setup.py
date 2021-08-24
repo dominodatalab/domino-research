55b3d64a70dd5ede8e3ee896a7395fd0bdc6e5c3
Kevin Flansburg <66090878+ddl-kevin@users.noreply.github.com>
2021-08-24 11:05:56 -0500

    PENG-619: Set up in-situ monitoring project (#42)
    * Create new project for in-situ monitoring
    
    * Restrict workflows by project path

---
+++ monitor/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="monitor",
+    version="0.1.0",
+    author="Kevin Flansburg, Josh Broomberg",
+    author_email="kevin.flansburg@dominodatalab.com,josh.broomberg@dominodatalab.com",
+    description="Lightweight model monitoring framework.",
+    url="https://github.com/dominodatalab/domino-research/monitor",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+)
