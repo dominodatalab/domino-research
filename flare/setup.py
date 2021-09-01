739ac266c12ab57dcd2f9a571299662ef2e32e7e
Kevin Flansburg <kevin.flansburg@dominodatalab.com>
2021-09-01 16:14:20 -0400

    Workflow for publishing Flare to PyPI

--- flare/setup.py
+++ flare/setup.py
@@ -1,8 +1,9 @@
 import setuptools
+import os
 
 setuptools.setup(
     name="flare",
-    version="0.1.0",
+    version=os.environ.get("RELEASE_VERSION", "SNAPSHOT"),
     author="Kevin Flansburg, Josh Broomberg",
     author_email="kevin.flansburg@dominodatalab.com,josh.broomberg@dominodatalab.com",
     description="Lightweight model monitoring framework.",
