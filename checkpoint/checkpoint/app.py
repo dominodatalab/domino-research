891889aecccc426a968cd67ef13db9cf44ac5ec6
Kevin Flansburg <kevin.flansburg@dominodatalab.com>
2021-09-03 09:23:19 -0400

    POC for major Flask routes

--- checkpoint/checkpoint/app.py
+++ checkpoint/checkpoint/app.py
@@ -1,2 +1,94 @@
-def main():
-    print("Hello World!")
+from flask import Flask
+from flask import request, Response, redirect, send_from_directory, send_file
+import requests
+import json
+import os
+from bs4 import BeautifulSoup
+
+app = Flask(__name__, static_url_path='/checkpoint/static', static_folder='../frontend/build/static')
+
+REGISTRY_URL = os.environ.get("CHECKPOINT_REGISTRY_URL", "http://mlflow.gambit-sandbox.domino.tech")
+
+INJECT_SCRIPT = """
+<script>
+function checkRequests () {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var requests = JSON.parse(this.responseText);
+            setRequests(requests);
+        }
+    };
+    req.open("GET", "/checkpoint/api/requests", true);
+    req.send();
+}
+
+function setRequests(requests) {
+    element = document.getElementById('requests-count');
+    element.innerHTML = requests.length;
+    if (requests.length === 0) {
+        element.classList.remove('ant-tag-red');
+    } else {
+        element.classList.add('ant-tag-red');
+    }
+}
+
+function checkRedirect() {
+    elements = document.getElementsByClassName("ant-message-error");
+    if (elements.length > 0) {
+        for (i = 0; i< elements.length; i++) {
+            if (elements[i].children[1].innerHTML == "Redirecting to Checkpoint") {
+                window.location = window.location.protocol + '//' + window.location.host + '/checkpoint/requests/new';
+            }
+        }
+    }
+}
+
+window.onload = function () {
+    element = document.getElementsByClassName("header-route-links")[0];
+    element.innerHTML += '<a class="header-nav-link header-nav-link-models" href="/checkpoint/requests"><div class="models"><span id="requests-count" class="ant-tag">-</span><span>Promote Requests</span></div></a>';
+    checkRequests();
+    setInterval(checkRequests, 5000);
+    setInterval(checkRedirect, 1000);
+}
+</script>
+"""
+
+@app.route('/ajax-api/2.0/preview/mlflow/model-versions/transition-stage', methods=["POST"])
+def intercept_tag():
+    return "Redirecting to Checkpoint"
+
+@app.route('/checkpoint/api/requests')
+def list_requests():
+    return json.dumps([{}])
+
+@app.route('/checkpoint/<path:path>')
+def spa_index(path):
+    return send_file('../frontend/build/index.html')
+
+@app.route('/', defaults={'path': ''}, methods=["GET", "POST", "PUT", "HEAD", "DELETE"])
+@app.route('/<path:path>', methods=["GET", "POST", "PUT", "HEAD", "DELETE"])
+def proxy(path):
+    resp = requests.request(
+        method=request.method,
+        url=f"{REGISTRY_URL}/{path}",
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False,
+        params=dict(request.args)
+    )
+
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    if resp.content.startswith(bytes("<!doctype html>", 'utf-8')):
+        soup = BeautifulSoup(resp.content.decode('utf-8'), 'html.parser')
+        print(soup.body.append(BeautifulSoup(INJECT_SCRIPT)))
+        content = str(soup).encode('utf-8')
+    else:
+        content = resp.content
+
+    response = Response(content, resp.status_code, headers)
+    return response
--- checkpoint/setup.py
+++ checkpoint/setup.py
@@ -8,6 +8,6 @@ setuptools.setup(
     description="Model approval layer for your model registry.",
     url="https://github.com/dominodatalab/domino-research/checkpoint",
     packages=setuptools.find_packages(),
-    install_requires=[],
+    install_requires=["Flask==2.0", "requests==2.26", "beautifulsoup4==4.9"],
     entry_points={"console_scripts": ["checkpoint = checkpoint.app:main"]},
 )
