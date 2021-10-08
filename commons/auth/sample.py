9eeddb26f8fbaf100435409e3479365c1be360be
Andrey Petrov <andrey.petrov@dominodatalab.com>
2021-10-08 17:40:59 -0500

    RND-140: Initial commit of common access control

---
+++ commons/auth/sample.py
@@ -0,0 +1,41 @@
+import os
+
+from flask import Flask  # type: ignore
+from flask import (
+    render_template,
+    url_for,
+)  # type: ignore
+
+from auth.client import LoginManager
+
+app = Flask(__name__)
+app.secret_key = os.urandom(16)  # Different key for every runtime
+
+login_manager = LoginManager(app)
+login_manager.callback_uri = "/auth"
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/login")
+def login():
+    return login_manager.login()
+
+
+@app.route("/auth")
+def auth_callback():
+    return login_manager.auth_callback()
+
+
+@app.route("/logout")
+def logout():
+    return login_manager.logout(url_for("home"))
+
+
+@app.route("/protected")
+@login_manager.auth_required
+def protected():
+    return render_template("protected.html")
